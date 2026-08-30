d3247bc5a47ee498fdaa348b6642221cdacf1f08
Akshay <akshayag229@gmail.com>
2026-08-31 00:41:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (22.54%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while(l<r):
+            m = l+(r-l) // 2
+            if m%2 == 1:
+                m -= 1
+            if nums[m] == nums[m+1]:
+                l = m + 2
+            else:
+                r = m
+        return nums[l]
