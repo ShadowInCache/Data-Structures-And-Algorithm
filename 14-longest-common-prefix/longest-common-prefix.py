ab2e7dcdd6258867d94dce2a81c4faa822d9eee9
Akshay <akshayag229@gmail.com>
2026-09-05 16:11:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (70.30%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -8,11 +8,10 @@ class Solution(object):
 
         for i in range(len(strs[0])):
             char = strs[0][i]
-        
-            for j in range(1, len(strs)):
+
+            for j in range(len(strs)):
                 if i >= len(strs[j]) or strs[j][i] != char:
                     return prefix
             
             prefix += char
         return prefix
-
